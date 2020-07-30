4a094334eb5a307a4ef24faa21d836081f9b95c3
grmelito <gr.melito@hotmail.com>
2020-07-29 23:45:06 -0300

    Adding Login Test Cases

---
+++ SeleniumCore/HomePageFeature.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using SeleniumCore.PageObjects;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SeleniumCore
+{
+    [TestClass]
+    public class HomepageFeature
+    {
+        
+        IWebDriver _driver;
+        [TestMethod]
+        public void ShouldBeAbleToLogin()
+        {
+             var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _driver = new FirefoxDriver(outPutDirectory);
+            var HomePO = new HomePO(_driver);
+            HomePO.Visit();
+            HomePO.FillForm("standard_user", "secret_sauce");
+            HomePO.SubmitForm();
+
+            Assert.IsTrue(_driver.Url.Contains("inventory.html"));
+        }
+
+        [TestMethod]
+        public void ShouldNotBeAbleToLogin()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _driver = new FirefoxDriver(outPutDirectory);
+            var HomePO = new HomePO(_driver);
+            HomePO.Visit();
+            HomePO.FillForm("WrongUser", "Admin1234");
+            HomePO.SubmitForm();
+
+           IWebElement err_message = _driver.FindElement(By.XPath("/html/body/div[2]/div[1]/div/div/form/h3"));
+            Assert.IsTrue(err_message.Displayed);
+        }
+
+    }
+}
---
+++ SeleniumCore/PageObjects/HomePO.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCore.PageObjects
+{
+    public class HomePO
+    {
+        private IWebDriver _driver;
+        private By userNameField;
+        private By passwordField;
+        private By loginButton;
+        private By err_message;
+
+        public HomePO(IWebDriver driver)
+        {
+            this._driver = driver;
+            userNameField = By.Id("user-name");
+            passwordField = By.Id("password");
+            loginButton = By.Id("login-button");
+            err_message = By.XPath("/html/body/div[2]/div[1]/div/div/form/h3");
+        }
+        public void Visit()
+        {
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com");
+        }
+        public void SubmitForm()
+        {
+            _driver.FindElement(loginButton).Click();
+        }
+        public void FillForm(string username, string password)
+        {
+            _driver.FindElement(userNameField).SendKeys(username);
+            _driver.FindElement(passwordField).SendKeys(password);
+           
+        }
+        public void FillFormWrong(string username, string password)
+        {
+            _driver.FindElement(userNameField).SendKeys(username);
+            _driver.FindElement(passwordField).SendKeys(password);
+
+        }
+    }
+}
