38c0dce3d837133554bc80eb966bf50822ce3be9
grmelito <gr.melito@hotmail.com>
2020-07-30 15:50:37 -0300

    Test Case for purchase

--- SeleniumCore/InventoryFeatures.cs
+++ SeleniumCore/InventoryFeatures.cs
@@ -15,15 +15,23 @@ namespace SeleniumCore
     {
         IWebDriver _driver;
         [TestMethod]
-        public void ShouldBeAbleToAddItems()
+        public void ShouldBeAbleToCompletePurchase()
         {
             var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             _driver = new FirefoxDriver(outPutDirectory);
             var InventoryPO = new InventoryPO(_driver);
+            var CartPO = new CartPO(_driver);
+            var CheckoutOnePO = new CheckoutOnePO(_driver);
+            var CheckoutTwoPO = new CheckoutTwoPO(_driver);
+
             InventoryPO.Visit();
             InventoryPO.addAllItemsToCart();
+            CartPO.confirmCart();
+            CheckoutOnePO.fillInformationForm("Andrei", "Gonçalves", "1109657");
+            CheckoutOnePO.submitForm();
+            CheckoutTwoPO.finishPurchase();
 
-            Assert.IsTrue(_driver.Url.Contains("cart.html"));
+            Assert.IsTrue(_driver.Url.Contains("checkout-complete.html"));
         }
     }
 }
