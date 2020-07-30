6ae658b8ef96b3862db37c127bdc35b5c0d1f71a
grmelito <gr.melito@hotmail.com>
2020-07-30 15:31:40 -0300

    Creating Checkout PageObjects

---
+++ SeleniumCore/PageObjects/CheckoutOnePO.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCore.PageObjects
+{
+    public class CheckoutOnePO
+    {
+        private IWebDriver _driver;
+        private By firstNameInput;
+        private By lastNameInput;
+        private By zipCodeInput;
+        private By continueButton;
+
+        public CheckoutOnePO(IWebDriver driver)
+        {
+            this._driver = driver;
+            firstNameInput = By.Id("first-name");
+            lastNameInput = By.Id("last-name");
+            zipCodeInput = By.Id("postal-code");
+            continueButton = By.ClassName("cart_button");
+        }
+
+        public void fillInformationForm(string name, string lastname, string zipcode)
+        {
+            _driver.FindElement(firstNameInput).SendKeys(name);
+            _driver.FindElement(lastNameInput).SendKeys(lastname);
+            _driver.FindElement(zipCodeInput).SendKeys(zipcode);
+        }
+
+        public void submitForm()
+        {
+            _driver.FindElement(continueButton).Click();
+        }
+    }
+}
---
+++ SeleniumCore/PageObjects/CheckoutTwoPO.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCore.PageObjects
+{
+    public class CheckoutTwoPO
+    {
+        private IWebDriver _driver;
+        private By finishButton;
+        private By cancelButton;
+
+        public CheckoutTwoPO(IWebDriver driver)
+        {
+            this._driver = driver;
+            finishButton = By.ClassName("cart_button");
+            cancelButton = By.ClassName("cart_cancel_link");
+        }
+        public void finishPurchase()
+        {
+            _driver.FindElement(finishButton).Click();
+        }
+
+        public void cancelPurchase()
+        {
+            _driver.FindElement(cancelButton).Click();
+        }
+    }
+}
