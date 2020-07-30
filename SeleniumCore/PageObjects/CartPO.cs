a34ed20b3e3a6d4574bd63730925edbf88842088
grmelito <gr.melito@hotmail.com>
2020-07-30 14:59:41 -0300

    Creating Cart PageObjects

---
+++ SeleniumCore/PageObjects/CartPO.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCore.PageObjects
+{
+    public class CartPO
+    {
+        private IWebDriver _driver;
+        private By checkoutButton;
+        private By continueShopButton;
+
+        public CartPO(IWebDriver driver)
+        {
+            this._driver = driver;
+            checkoutButton = By.ClassName("checkout_button");
+            continueShopButton = By.ClassName("btn_secondary");
+        }
+        public void confirmCart()
+        {
+            _driver.FindElement(checkoutButton).Click();
+            
+        }
+        public void continueShopping()
+        {
+            _driver.FindElement(continueShopButton).Click();
+        }
+    }
+    
+}
