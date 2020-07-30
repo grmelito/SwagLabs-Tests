88e7f8c57827a153b40273effcb9e5bc189a5930
grmelito <gr.melito@hotmail.com>
2020-07-30 14:38:39 -0300

    Creating Inventory PageObjects

---
+++ SeleniumCore/InventoryFeatures.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using SeleniumCore.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SeleniumCore
+{
+    [TestClass]
+    public class InventoryFeatures
+    {
+        IWebDriver _driver;
+        [TestMethod]
+        public void ShouldBeAbleToAddItems()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _driver = new FirefoxDriver(outPutDirectory);
+            var InventoryPO = new InventoryPO(_driver);
+            InventoryPO.Visit();
+            InventoryPO.addAllItemsToCart();
+
+            Assert.IsTrue(_driver.Url.Contains("cart.html"));
+        }
+    }
+}
---
+++ SeleniumCore/PageObjects/InventoryPO.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCore.PageObjects
+{
+    public class InventoryPO
+    {
+        private IWebDriver _driver;
+        private By addCartButton;
+        private By checkCartButton;
+
+        public InventoryPO(IWebDriver driver)
+        {
+            this._driver = driver;
+            addCartButton = By.ClassName("btn_inventory");
+            checkCartButton = By.XPath("//*[name()='path' and contains(@fill,'currentCol')]");
+        }
+        public void Visit()
+        {
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com/inventory.html");
+        }
+        public void addAllItemsToCart()
+        {
+            _driver.FindElement(addCartButton).Click();
+            _driver.FindElement(checkCartButton).Click();
+        }
+
+
+    }
+}
